5f67dd99118d5b05fc9abaf5b71dd98e1cfbfa01
hungrybunny <daphne@hungrybunny.com>
2014-04-01 11:59:46 -0700

    landing and thank you pages

---
+++ FWV/images/thanks/stories-cbb9f55306171c95d74b4d6a39c67097.js
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>The page you were looking for doesn't exist (404)</title>
+  <meta charset='utf-8' />
+  <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible' />
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1' name='viewport' />
+  <style type="text/css">
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, section, summary,
+    time, mark, audio, video {
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 100%;
+      font: inherit;
+      vertical-align: baseline;
+    }
+    body { 
+      margin: 0;
+      padding: 0;
+      font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+      line-height: 1;
+      text-decoration: none;
+      text-rendering: optimizelegibility;
+      background-color: #fff;
+      color: #666;
+    }
+    div.dialog {
+      width: 21em;
+      padding: 4em;
+      margin: 12em auto 0 auto;
+      border: 2px solid rgb(230, 230, 230);
+      background-color: rgb(245, 245, 245);
+      -webkit-border-radius: 6px; border-radius: 6px;
+      -moz-background-clip: padding; -webkit-background-clip: padding-box; background-clip: padding-box;
+    }
+    h1 { 
+      margin-bottom: 8px;
+      color: rgb(50, 50, 50); 
+      line-height: 1.5em;  
+      font-size: 92px; 
+      line-height: 80px; 
+      font-weight: bold;
+    }
+    p{
+      color: rgb(78, 78,78);
+      display: block;
+      font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+      font-size: 20px;
+      font-weight: 300;
+      line-height: 32px;
+      margin: 0 0 10px 0;
+      width: auto;
+    }
+    a{
+      color: rgb(63, 186, 223);
+      text-decoration: none;
+      font-size: 16px;
+      line-height: 24px;
+    }
+    a:hover{
+      color: #08C;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- This file lives in public/consumer-errors/404.html -->
+  <div class="dialog">
+    <h1 class="status">404</h1>
+    <p>Sorry, the page you're looking for could not be found.</p>
+  </div>
+</body>
+</html>
